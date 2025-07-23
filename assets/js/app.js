45529fd653b577afd15fafe6cc3c6d91f1095205
Michael Barrientos <michaelbarrientos788@gmail.com>
2025-07-22 22:47:10 -0400

    feat: added clock functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 
+// theme toggle variables
 const themeToggleButton = document.getElementById('theme-toggle');
 const body = document.body;
 
@@ -9,3 +10,21 @@ const body = document.body;
 themeToggleButton.addEventListener('click', () => {
   body.classList.toggle('light-mode'); 
 })
+
+/**
+ * Clock
+ */
+
+// FIX: timeString assumes the locale string will be five characters long, however this is not the case with 
+// times of the form 0H:MM
+function updateClock() {
+  const currentTime = Temporal.Now.plainTimeISO();
+  const timeString = currentTime.toLocaleString().slice(0, 5);
+  document.getElementById("clock").textContent = timeString;
+
+  // This improves accuracy by having updateClock() correct itself each time it is called
+  const msUntilNextSecond = 1000 - currentTime.millisecond;
+  setTimeout(updateClock, msUntilNextSecond);
+}
+
+updateClock();
